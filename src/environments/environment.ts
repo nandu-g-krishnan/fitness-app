d5fc9b2bb1f13141d2992b231eba3c9270c9becb
NGK <59110984+nandu-g-krishnan@users.noreply.github.com>
2020-03-22 23:23:31 +0530

    environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAgsK1Is0QwGwhCQsKAm5nsAcYxcez6vGU',
-    authDomain: 'ng-fitness-tracker.firebaseapp.com',
-    databaseURL: 'https://ng-fitness-tracker.firebaseio.com',
-    projectId: 'ng-fitness-tracker',
-    storageBucket: 'ng-fitness-tracker.appspot.com',
-    messagingSenderId: '183546960271'
+    apiKey: "AIzaSyC9Okc9gdXiZIWHqdXsCxUBMqFzffdpUoE",
+    authDomain: "ng-fitness-tracker-63784.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-63784.firebaseio.com",
+    projectId: "ng-fitness-tracker-63784",
+    storageBucket: "ng-fitness-tracker-63784.appspot.com",
+    messagingSenderId: "221312543993",
+    appId: "1:221312543993:web:6105aa8aba6db0051c4628",
+    measurementId: "G-W8JEE9KL6X"
   }
 };
